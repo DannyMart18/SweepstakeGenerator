acdc1d527134d40f345863faaecc02021a0e2c5f
DannyMart18 <dannymart18@hotmail.com>
2024-05-06 14:34:25 +0100

    HORSES

--- SweepstakeGeneratorv2/Services/SweepstakeService.cs
+++ SweepstakeGeneratorv2/Services/SweepstakeService.cs
@@ -6,20 +6,40 @@ public class SweepstakeService: ISweepstakeService
 {
     private static readonly List<Horse> AllHorses = new List<Horse>
     {
-        //test data
-        // Assuming these are the horses. Replace with real data.
-        new Horse { Name = "Horse 1" },
-        new Horse { Name = "Horse 2" },
-        new Horse { Name = "Horse 3" },
-        new Horse { Name = "Horse 4" },
-        new Horse { Name = "Horse 5" },
-        new Horse { Name = "Horse 6" },
-        new Horse { Name = "Horse 7" },
-        new Horse { Name = "Horse 8" },
-        new Horse { Name = "Horse 9" },
-        new Horse { Name = "Horse 10" },
-        new Horse { Name = "Horse 11" },
+        new Horse { Name = "1 Noble Yeats" },
+        new Horse { Name = "2 Nassalam" },
+        new Horse { Name = "3 Coko Beach" },
+        new Horse { Name = "4 Capodanno" },
+        new Horse { Name = "5 I Am Maximus" },
+        new Horse { Name = "6 Minella Indo" },
+        new Horse { Name = "7 Corach Rambler" },
+        new Horse { Name = "8 Janidil" },
+        new Horse { Name = "9 Stattler" },
+        new Horse { Name = "10 Mahler Mission" },
+        new Horse { Name = "11 Delta Work" },
+        new Horse { Name = "12 Foxy Jacks" },
+        new Horse { Name = "13 Galvin" },
+        new Horse { Name = "14 Farouk d'Alene" },
+        new Horse { Name = "15 Eldorado Allen" },
+        new Horse { Name = "16 Ain't That A Shame" },
+        new Horse { Name = "17 Vanillier" },
+        new Horse { Name = "18 Mr Incredible" },
+        new Horse { Name = "20 Latenightpass" },
+        new Horse { Name = "21 Minella Crooner" },
+        new Horse { Name = "22 Adamantly Chosen" },
+        new Horse { Name = "23 Mac Tottie" },
+        new Horse { Name = "24 Chemical Energy" },
+        new Horse { Name = "25 Limerick Lace" },
+        new Horse { Name = "26 Meetingofthewaters" },
+        new Horse { Name = "27 The Goffer" },
+        new Horse { Name = "28 Roi Mage" },
+        new Horse { Name = "29 Glengouly" },
+        new Horse { Name = "30 Galia Des Liteaux" },
+        new Horse { Name = "31 Panda Boy" },
+        new Horse { Name = "32 Eklat De Rire" },
+        new Horse { Name = "34 Kitty's Light" }
     };
+
     
     public List<Person> GenerateSweepstake(List<Person> person)
     {
